25ce76d084df0c90d49cea3b9fa4855f6c951885
CamClendenon <camilleclendenon@gmail.com>
2023-04-23 18:58:01 -0700

    Added LogIn Activity
    Added LogIn Activity

---
+++ app/src/main/java/com/example/cst338_p2_movie_schmovies/log_In_Page.java
@@ -0,0 +1,14 @@
+package com.example.cst338_p2_movie_schmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class log_In_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_page);
+    }
+}
