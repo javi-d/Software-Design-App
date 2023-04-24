3a29aed2fbf39c0f6561a86d00dd637fef4f01df
Javier Dominguez <116839262+javi-d@users.noreply.github.com>
2023-04-23 21:20:47 -0700

    Updated MainActivity
    Updated MainActivity

--- app/src/main/java/com/example/cst338_p2_movie_schmovies/MainActivity.java
+++ app/src/main/java/com/example/cst338_p2_movie_schmovies/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.cst338_p2_movie_schmovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.cst338_p2_movie_schmovies.DB.DAO;
+import com.example.cst338_p2_movie_schmovies.databinding.ActivityMainBinding;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    ActivityMainBinding mBinding;
 
+    TextView mTitle;
 
+    Button mLogin;
+    Button mSignup;
+    Button mContinueAsGuest;
+
+    DAO mDAO;
+
+    List<Users> mUsersList;
 
 
     @Override
