a90c3dcc4b849abe7a143e475df2ecc20cf3f4aa
CamClendenon <camilleclendenon@gmail.com>
2023-05-05 16:50:04 -0700

    Created base information for Movie class

---
+++ app/src/main/java/com/example/cst338_p2_movie_schmovies/Movie.java
@@ -0,0 +1,82 @@
+package com.example.cst338_p2_movie_schmovies;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Entity()
+public class Movie {
+
+    @PrimaryKey(autoGenerate = true)
+    private int mLogId;
+    private String mMovieName;
+    private String mSynopsis;
+
+    private String mAgeRating;
+
+    private HashMap<String, List<String>> theaterShowTimes; // String for Theater location/name and String List for movie Times at that Theater.
+
+    //````````````````````````````````````````````Constructor and toString()`````````````````````````````````````````//
+
+    public Movie(String movieName, String synopsis, String ageRating) {
+        mMovieName = movieName;
+        mSynopsis = synopsis;
+        mAgeRating = ageRating;
+        theaterShowTimes = new HashMap<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "mLogId=" + mLogId +
+                ", mMovieName='" + mMovieName + '\'' +
+                ", mSynopsis='" + mSynopsis + '\'' +
+                ", mAgeRating='" + mAgeRating + '\'' +
+                ", theaterShowTimes=" + theaterShowTimes +
+                '}';
+    }
+//````````````````````````````````````````````Getter and Setters````````````````````````````````````//
+
+
+    public int getLogId() {
+        return mLogId;
+    }
+
+    public void setLogId(int logId) {
+        mLogId = logId;
+    }
+
+    public String getMovieName() {
+        return mMovieName;
+    }
+
+    public void setMovieName(String movieName) {
+        mMovieName = movieName;
+    }
+
+    public String getSynopsis() {
+        return mSynopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        mSynopsis = synopsis;
+    }
+
+    public String getAgeRating() {
+        return mAgeRating;
+    }
+
+    public void setAgeRating(String ageRating) {
+        mAgeRating = ageRating;
+    }
+
+    public HashMap<String, List<String>> getTheaterShowTimes() {
+        return theaterShowTimes;
+    }
+
+    public void setTheaterShowTimes(HashMap<String, List<String>> theaterShowTimes) {
+        this.theaterShowTimes = theaterShowTimes;
+    }
+}
